6533dcc67465c10b684bca72d77d5710669f9674
timur <tymur1404@gmail.com>
2019-11-22 17:12:21 +0200

    all messages in view

--- resources/views/blog/admin/posts/includes/result_messages.blade.php
+++ resources/views/blog/admin/posts/includes/result_messages.blade.php
@@ -5,7 +5,10 @@
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">x</span>
             </button>
-            {{ $errors->first() }}
+            <!-- вывод всех ошибок -->
+            @foreach($errors->all() as $errorText)
+                <li>{{ $errorText }}</li>
+            @endforeach
         </div>
     </div>
 </div>
